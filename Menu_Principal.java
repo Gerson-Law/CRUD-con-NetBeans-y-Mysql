03c8d114561e2cee01fc8e92298104aa6b9d7bf5
Gerson <82684569+Gerson-Law@users.noreply.github.com>
2021-05-08 13:58:37 -0600

    Add files via upload

---
+++ Menu_Principal.java
@@ -0,0 +1,53 @@
+package registro_estudiantes;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Menu_Principal {
+   public static void main(String[] args) throws SQLException {
+          desplegarMenu();
+    }
+    
+    public static void desplegarMenu() throws SQLException {
+    Scanner opSeleccionada = new Scanner(System.in);
+    String opMenu;
+    
+        System.out.println("||========================================||");
+        System.out.println("||                                        ||");
+        System.out.println("||=======> MENU CRUD ESTUDIANTES <========||");
+        System.out.println("||                                        ||");
+        System.out.println("||========================================||");
+        System.out.println("||                                        ||");
+        System.out.println("||::::::::::  MENU DE OPCIONES :::::::::::||");
+        System.out.println("||  1.Crear Registro de estudiante        ||");
+        System.out.println("||  2.Consultar Registros de estudiantes  ||");
+        System.out.println("||  3.Actualizar Registro de estudiante   ||");
+        System.out.println("||  4.Eliminar Registro de estudiante     ||");
+        System.out.println("||  5.Salir                               ||");
+        System.out.println("||                                        ||");
+        System.out.println("||========================================||");
+        System.out.println("||  SELECCIONE EL METODO CRUD A UTLIZAR   ||");
+        opMenu = opSeleccionada.next();
+        
+        switch (opMenu) {
+            case "1":
+                Crear create = new Crear();
+                break;
+            case "2":
+                Leer read = new Leer();
+                break;
+            case "3":
+                Actualizar update = new Actualizar();
+                break;
+            case "4":
+                Eliminar delete = new Eliminar();
+                break;
+            case "5":
+                System.exit(0);
+                break;
+            default:
+                System.out.println("LA OPCION NO EXISTE!!!");
+        }
+    }   
+}
+
